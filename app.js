39b57908aa2f03835df3a5305b8dbc483ae0b993
lbrutti-Pi <lorenzobrutti80@gmail.com>
2019-10-12 19:22:16 +0100

    inserito invio mesaggi OSC e ricezione in patch PD

--- app.js
+++ app.js
@@ -2,34 +2,52 @@ const Noble = require("noble");
 const BeaconScanner = require("node-beacon-scanner");
 let scanner = new BeaconScanner();
 
-const soundplayer = require("sound-player");
-let options = {
-    filename: "loops/ding.wav",
-    gain: 100,
-    debug: false,
-    player: "aplay", // "afplay" "aplay" "mpg123" "mpg321"
-    device: "plughw:0,0"   //
-}
 
-let isPlaying = false;
+let osc = require("osc");
 
-let player = new soundplayer(options)
+let udpPort = new osc.UDPPort({
+    // This is the port we're listening on.
+    localAddress: "127.0.0.1",
+    localPort: 57121,
 
-player.on('complete', function () {
-    console.log('Done with playback!');
-    isPlaying = false;
+    // This is where sclang is listening for OSC messages.
+    remoteAddress: "127.0.0.1",
+    remotePort: 57120,
+    metadata: true
 });
 
-player.on('error', function (err) {
-    console.log('Error occurred:', err);
-});
+
+// Open the socket.
+udpPort.open();
+
+// const soundplayer = require("sound-player");
+// let options = {
+//     filename: "loops/ding.wav",
+//     gain: 100,
+//     debug: false,
+//     player: "aplay", // "afplay" "aplay" "mpg123" "mpg321"
+//     device: "plughw:0,0"   //
+// }
+
+// let isPlaying = false;
+
+// let player = new soundplayer(options)
+
+// player.on('complete', function () {
+//     console.log('Done with playback!');
+//     isPlaying = false;
+// });
+
+// player.on('error', function (err) {
+//     console.log('Error occurred:', err);
+// });
 scanner.onadvertisement = (advertisement) => {
     if (advertisement["beaconType"] == "eddystoneTlm") {
         try {
-            if (!isPlaying) {
-                isPlaying = true;
-                player.play();
-            }
+            // if (!isPlaying) {
+            //     isPlaying = true;
+            //     player.play();
+            // }
             console.log(JSON.stringify(advertisement, null, "    "))
 
         } catch (e) { console.error(e) }
@@ -42,3 +60,17 @@ scanner.startScan().then(() => {
     console.error(error);
 });
 
+
+
+
+// Every second, send an OSC message to SuperCollider
+setInterval(function() {
+    var msg = {
+        address: "/oscjs",
+        args: [{type:"f", value:1}]
+        
+    };
+
+    console.log("Sending message", msg.address, msg.args, "to", udpPort.options.remoteAddress + ":" + udpPort.options.remotePort);
+    udpPort.send(msg);
+}, 1000);
